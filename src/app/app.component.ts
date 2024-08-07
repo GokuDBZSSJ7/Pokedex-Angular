8d0d7ace8f2f259d7a533a2ef5bf87592d66d45a
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-08-07 19:31:22 -0300

    Feat: First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonService } from './services/pokemon.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pokedex';
+  pokemon: any[] = []
+
+  constructor(private pokeService: PokemonService) { }
+
+  list() {
+    this.pokeService.all().subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.pokemon = res
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.list()
+  }
 }
