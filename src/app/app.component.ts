196711e24e7b9910a3462fbbaf91db191d42124c
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-08-07 21:54:31 -0300

    Feat: Implementação dos Pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PokemonService } from './services/pokemon.service';
+import { HeaderComponent } from './shared/header/header.component';
+import { NgClass, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UpperCasePipe, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'Pokedex';
-  pokemon: any[] = []
+  pokemonList: any[] = [];
+
+  colors = ["Red", "Blue", "White"]
 
   constructor(private pokeService: PokemonService) { }
 
   list() {
-    this.pokeService.all().subscribe({
-      next: (res) => {
-        console.log(res);
-
-        this.pokemon = res
-      }
+    this.pokeService.all().subscribe(data => {
+      console.log(data);
+      
+      this.pokemonList = data
     })
   }
 
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,16 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  path = 'https://pokeapi.co/api/v2'
+  path = 'https://pokeapi.co/api/v2/pokemon'
 
 
-  all(): Observable<any> {
-    return this.http.get(`${this.path}`);
+  all(limit: number = 151): Observable<any[]> {
+    return this.http.get<any>(`${this.path}?limit=${limit}`).pipe(
+      switchMap(data => {
+        const pokemonRequests: Observable<any>[] = data.results.map((pokemon: any) => this.http.get<any>(pokemon.url));
+        return forkJoin(pokemonRequests);
+      })
+    );
   }
+  
 }
